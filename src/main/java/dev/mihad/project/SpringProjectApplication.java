714365553d0575f9bbd725f0296a144a42de7272
mihad-khadem <mihadkhadem@gmail.com>
2025-01-16 23:11:16 +0600

    java spring rest api

--- src/main/java/dev/mihad/project/SpringProjectApplication.java
+++ src/main/java/dev/mihad/project/SpringProjectApplication.java
@@ -1,7 +1,12 @@
 package dev.mihad.project;
 
+
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringProjectApplication {
@@ -9,5 +14,16 @@ public class SpringProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringProjectApplication.class, args);
 	}
+
+
+	// hello controller
+	@RestController
+	 public class HelloController {
+	 // get mapping
+	@GetMapping("/hello")
+	public List<String> getHello() {
+		return List.of("hello","world")	;
+	}
+ }
 
 }
